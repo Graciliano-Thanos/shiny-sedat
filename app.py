d23e60f73315615d5dc41fe1a2a88538b327ea33
Graciliano-Thanos <guilherme.rosa.bks@gmail.com>
2023-04-09 09:18:28 -0300

    teste estrutura shiny

---
+++ app.py
@@ -0,0 +1,16 @@
+from shiny import App, render, ui
+
+app_ui = ui.page_fluid(
+    ui.input_slider("n", "N", 0, 100, 20),
+    ui.output_text_verbatim("txt"),
+)
+
+
+def server(input, output, session):
+    @output
+    @render.text
+    def txt():
+        return f"n*2 is {input.n() * 2}"
+
+if __name__ == "__main__":
+    app = App(app_ui, server)
