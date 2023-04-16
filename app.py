56fa3fda93f8936a1f8787ccd8304bad6375e021
Graciliano-Thanos <guilherme.rosa.bks@gmail.com>
2023-04-16 16:40:54 -0300

    primeira versao view sedat feita

--- app.py
+++ app.py
@@ -3,32 +3,25 @@ from shiny import App, render, ui, reactive
 from shinywidgets import output_widget, reactive_read, register_widget
 from ipyleaflet import Map, Marker, Popup,leaflet
 
-from read_db import find_db
+from read_db import find_db,info_from_db
 
 app_ui = ui.page_fluid(
     
-    ui.div(
+    ui.layout_sidebar(
+        ui.panel_sidebar(
     ui.input_select("db","Choose a database:",["USA","Global"]),
     # Filters for both databases
-    ui.input_selectize("filter_1", "Filter 1", [],multiple=True),
-    ui.input_selectize("filter_2", "Filter 2", [],multiple=True),
-    ui.input_selectize("filter_3", "Filter 3", [],multiple=True),
-    ui.input_selectize("filter_4", "Filter 4", [],multiple=True),
-    ui.input_selectize("filter_5", "Filter 5", [],multiple=True),
-    ui.output_table("table"),
-    style=css(
-            display="flex", justify_content="left", align_items="center", gap="2rem"
-        ),
+    ui.input_selectize("filter_1", "Select Size", [],multiple=True),
+    ui.input_selectize("filter_2", "Select Technology", [],multiple=True),
+    ui.input_selectize("filter_3", "Select Plant Type", [],multiple=True),
+    ui.input_slider("zoom", "Map zoom level", value=3, min=1, max=18),
+    ui.input_action_button("run","Run Filtered Database")
     ),
-    
-    ui.div(
-        ui.input_slider("zoom", "Map zoom level", value=3, min=1, max=18),
-        ui.output_ui("map_bounds"),
-        style=css(
-            display="flex", justify_content="center", align_items="center", gap="2rem"
-        ),
-    ),
+    ui.panel_main(
+    ui.output_ui("map_bounds"),
     output_widget("map")
+    )
+    ),
 )
 
 def server(input, output, session):
@@ -48,31 +41,41 @@ def server(input, output, session):
     def _():
         ui.update_slider("zoom", value=reactive_read(map, "zoom"))
 
-    @output
-    def widget():
+    @reactive.Effect
+    def _():
         db, filters = find_db(input.db())
 
         #UPDATE FILTERS BASED ON DB
         for i in range(1,len(filters)+1):
             ui.update_select(
             f"filter_{i}",
-            label=f"Select {filters[i-1]}",
             choices=list(db[filters[i-1]].unique()),
             selected=None,)
 
-        # SET filters
+    @reactive.Effect
+    @reactive.event(input.run)
+    def _():
+        db, filters = find_db(input.db())
 
-        for sub_filter in filters:
-            db = db[db[sub_filter].isin(input.filter1())]
+        filter_table = [input.filter_1(),input.filter_2(),input.filter_3()]
 
-        for dessal in db:
-            marker = Marker(location=(dessal['latitude'], dessal['longitude']))
-            #message = HTML()
-            #message.value = "Description of Dessal:"
-            #message.description = info_from_database()   -> to be made
-            map.add_layer(marker)
-            #marker.popup = message
-        return db.to_html()
+        for i in range(len(filters)):
+            db = db[db[filters[i]] == filter_table[i][0]]
+
+        print(db)
+
+        print("-----------------------------------")
+        for ind in db.index:
+           #print(db[ind])
+           marker = Marker(location=(db['Latitude'][ind], db['Longitude'][[ind]]),draggable=False)
+           #message = HTML()
+           #message.value = "Description of Dessal:"
+           #message.description = info_from_db("dessal")
+           map.add_layer(marker)
+           #marker.popup = message
+
+        return db
+
     
 
 app = App(app_ui, server)
--- read_db.py
+++ read_db.py
@@ -5,6 +5,9 @@ def find_db(path:str):
     infile = Path(__file__).parent / f"{path}.csv"
     
     db = pd.read_csv(infile,low_memory=False)
-    filters = ["Country","Location","Size","Technology","Plant type" if path == "Global" else "Plant_type"]
+    filters = ["Size","Technology","Plant type" if path == "Global" else "Plant_type"]
 
     return db, filters
+
+def info_from_db(*args):
+    return "text teste."
